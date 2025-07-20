a53dabb890b26598e7e9ec5916b6810f3ffe9300
aerdnassi <ejcanatoy@protonmail.com>
2025-07-21 00:26:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,4 +32,5 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon="✅")
 
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
